f7ca6544a2cd14b406840d0995caad8f4bd786d7
Liuba <pl@mail.ru>
2022-09-17 15:51:54 +0300

    ДЗ: Задача 2

--- 001Task/Program.cs
+++ 001Task/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Программа, которая на вход принимает два числа и выдаёт, какое число большее
+Console.Clear();
+Console.WriteLine("Введите число 1: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x == y)
+{
+   Console.Write("Числа равны");
+}
+else
+{
+   if (x < y)
+   {
+      Console.Write("max = ");
+      Console.WriteLine(y);
+   }
+   else
+   {
+      Console.Write("max = ");
+      Console.WriteLine(y);
+   }
+}

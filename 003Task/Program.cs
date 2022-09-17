5840b7188e61c991a78e9d8f883229138f120bc5
Liuba <pl@mail.ru>
2022-09-17 17:35:19 +0300

    ДЗ: Задача 6.

---
+++ 003Task/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 == 0)
+{
+   Console.Write("Число четное");
+}
+else
+{
+   Console.Write("Число нечетное");
+}

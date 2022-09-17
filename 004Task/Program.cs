1d2ec4a69a109e4a70a1fde6c16369a26eda30c4
Liuba <pl@mail.ru>
2022-09-17 18:06:57 +0300

    ДЗ: Задача 8.

---
+++ 004Task/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Четные числа от 1 до введенного числа: ");
+for (int i = 1; i <= x; i++)
+   if (i % 2 == 0)
+   {
+      Console.WriteLine(i);
+   }
